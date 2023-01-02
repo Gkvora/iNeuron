f95fd26078b2a66e5db760b96fd329cb3e202299
GAUTAM VORA <117502951+Gkvora@users.noreply.github.com>
2023-01-02 12:47:18 -0500

    Add files via upload

---
+++ ASSIGNMENT 2.py
@@ -0,0 +1,26 @@
+#Que.1
+print("Hello Python")
+
+#Que.2
+a=5
+b=2
+print(a+b)
+print(a-b)
+
+#Que.3
+Base=24
+Height=12
+area_of_triangle=(Base * Height)/2
+print(area_of_triangle)
+
+#Que.4
+x=12
+y=14
+x,y = y,x
+print(x)
+print(y)
+
+ #Que.5
+import random
+a = random.random()
+print(a)
